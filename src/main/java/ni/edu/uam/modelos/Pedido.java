e9d62e6a1504dad4d16b02437137e7f936830b5f
kellys bellanger <kbbellanger@uamv.edu.ni>
2026-04-28 23:10:40 -0600

    Agrego mejoras a la clase Pedido|Cafeteria

--- src/main/java/ni/edu/uam/modelos/Pedido.java
+++ src/main/java/ni/edu/uam/modelos/Pedido.java
@@ -7,17 +7,18 @@ public class Pedido {
     private List<Producto> productos;
     private String estado;
 
-    public Pedido(List<Producto> productos, String estado) {
-        this.productos = productos;
-        this.estado = estado;
+    public Pedido() {
+        this.productos = new ArrayList<>();
+        this.estado = "Pendiente";
     }
     //Metodo para agregar productos a la lista
     public void agregarProducto(Producto p) {
         productos.add(p);
     }
+
     //Metodo que cambia el estado
     public void cambiarEstado(String nuevoEstado) {
         this.estado = nuevoEstado;
-        System.out.println("LOG -> Estado del pedido actualizado a: " + estado);
+        System.out.println("-> Estado del pedido actualizado a: " + nuevoEstado);
     }
 }
